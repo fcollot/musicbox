5d33879c977e2d9cddfe08d4795e5a956ec2ee04
Florent Collot <florent.collot@ihu-liryc.fr>
2024-06-28 15:43:45 +0200

    foo

--- source/cpp_api/musicbox/conversion.h
+++
@@ -1,4 +0,0 @@
-// Copyright (c) 2024 IHU Liryc, Université de Bordeaux, Inria.
-// License: BSD-3-Clause
-
-#include "conversion/qobject.h"
--- source/python_api/musicbox/__main__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2024 IHU Liryc, Université de Bordeaux, Inria.
-# License: BSD-3-Clause
-
-
-from . import run
-
-
-run()
